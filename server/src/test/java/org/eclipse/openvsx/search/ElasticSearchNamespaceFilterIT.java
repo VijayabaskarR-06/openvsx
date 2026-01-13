fe5b8fbec2af3d92eb68fa3fe0115011e902a385
Vijayabaskar R <tbkoogle@gmail.com>
2026-01-13 10:44:25 +0530

    Add missing package and imports for namespace filter IT

--- server/src/test/java/org/eclipse/openvsx/search/ElasticSearchNamespaceFilterIT.java
+++ server/src/test/java/org/eclipse/openvsx/search/ElasticSearchNamespaceFilterIT.java
@@ -1,36 +1,58 @@
-@Test
-void namespaceFilterMatchesOnlyExactNamespace() {
+package org.eclipse.openvsx.search;
 
-    Extension vscode = new Extension();
-    vscode.setNamespace("vscode");
-    vscode.setName("python");
-    repositoryService.save(vscode);
+import static org.assertj.core.api.Assertions.assertThat;
 
-    Extension msVscode = new Extension();
-    msVscode.setNamespace("ms-vscode");
-    msVscode.setName("cpptools");
-    repositoryService.save(msVscode);
+import java.util.List;
 
-    // Rebuild the index so both extensions are searchable
-    elasticSearchService.updateSearchIndex(true);
+import org.eclipse.openvsx.entities.Extension;
+import org.eclipse.openvsx.repositories.RepositoryService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-    // Search using an exact namespace filter
-    Options options = new Options(
-            null,
-            "vscode",
-            null,
-            null,
-            null,
-            0,
-            10,
-            "desc",
-            SortBy.RELEVANCE
-    );
+@SpringBootTest
+@ActiveProfiles("test")
+class ElasticSearchNamespaceFilterIT {
 
-    SearchResult result = elasticSearchService.search(options);
+    @Autowired
+    private ElasticSearchService elasticSearchService;
 
-    assertThat(result.extensions())
-            .extracting(ExtensionSearch::getNamespace)
-            .contains("vscode")
-            .doesNotContain("ms-vscode");
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @Test
+    void namespaceFilterMatchesOnlyExactNamespace() {
+        Extension vscode = new Extension();
+        vscode.setNamespace("vscode");
+        vscode.setName("python");
+        repositoryService.save(vscode);
+
+        Extension msVscode = new Extension();
+        msVscode.setNamespace("ms-vscode");
+        msVscode.setName("cpptools");
+        repositoryService.save(msVscode);
+
+        elasticSearchService.updateSearchIndex(true);
+
+        Options options = new Options(
+                null,
+                "vscode",
+                null,
+                null,
+                null,
+                0,
+                10,
+                "desc",
+                SortBy.RELEVANCE
+        );
+
+        SearchResult result = elasticSearchService.search(options);
+        List<ExtensionSearch> extensions = result.extensions();
+
+        assertThat(extensions)
+                .extracting(ExtensionSearch::getNamespace)
+                .contains("vscode")
+                .doesNotContain("ms-vscode");
+    }
 }
